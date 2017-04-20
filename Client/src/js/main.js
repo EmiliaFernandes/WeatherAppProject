607ced9de45d106b837a0da9df851a34331d98cc
Emilia <Emilia>
2017-04-20 20:27:26 +0530

    first commit

---
+++ Client/src/js/main.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var WeatherApp=require('./components/WeatherApp.js');
+
+ReactDOM.render(<WeatherApp/>,document.getElementById('app'));
